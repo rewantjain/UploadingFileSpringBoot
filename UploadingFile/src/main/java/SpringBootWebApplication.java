a3c5659719f705390abca244f62983fea1c58ebc
rewantjain <rewantjain@gmail.com>
2017-04-18 10:29:12 -0400

    Files
    This project is to upload files using Spring Boot Java

---
+++ UploadingFile/src/main/java/SpringBootWebApplication.java
@@ -0,0 +1,14 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://www.agilegroup.co.jp/technote/springboot-fileupload-error-handling.html
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
